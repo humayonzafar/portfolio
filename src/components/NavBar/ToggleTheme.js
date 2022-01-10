d95529f274e6bcf2d7547b3b2673168b898ea839
Humayon Zafar <humayonhere@gmail.com>
2022-01-11 00:07:42 +0500

    Added tooltip and on off sound for toogle theme

--- src/components/NavBar/ToggleTheme.js
+++ src/components/NavBar/ToggleTheme.js
@@ -1,11 +1,27 @@
-import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { IconButton, useColorMode, useColorModeValue, Tooltip } from '@chakra-ui/react'
 import { BiMoon, BiSun } from 'react-icons/bi'
+import useSound from "use-sound";
 
 const ToggleTheme = ()=> {
-    const text = useColorModeValue('dark', 'light')
-    const SwitchIcon = useColorModeValue(BiMoon, BiSun)
-    const { colorMode, toggleColorMode } = useColorMode()
+    const text = useColorModeValue('dark', 'light');
+    const SwitchIcon = useColorModeValue(BiMoon, BiSun);
+    const { colorMode, toggleColorMode } = useColorMode();
+    const [play] = useSound("lightswitch.mp3", {
+        volume: 0.05,
+        sprite: {
+            on: [0, 300],
+            off: [500, 300],
+        },
+    });
+    const handleClick = () => {
+        toggleColorMode();
+        colorMode === "dark" ? play({ id: "on" }) : play({ id: "off" });
+    };
     return (
+        <Tooltip
+            label={colorMode === "dark" ? "Light mode" : "Dark mode"}
+            aria-label="A tooltip"
+        >
         <IconButton
             mr={{ base: 2, md: 0 }}
             size='md'
@@ -13,10 +29,10 @@ const ToggleTheme = ()=> {
             aria-label={`Switch to ${text} mode`}
             title={`Switch to ${text} mode`}
             variant='ghost'
-            onClick={toggleColorMode}
             icon={<SwitchIcon size={25} />}
-        >test
-        </IconButton>
+            onClick={handleClick}
+        />
+        </Tooltip>
     )
 }
 
